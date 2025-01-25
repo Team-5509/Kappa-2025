455b0c9c0062eac0e6a72708bf39aa7d4c4442d1
DESKTOP-HBJJU88\Robotics <mohawk1712@gmail.com>
2025-01-25 12:27:33 -0500

    Added some more Commands and Subsystems

---
+++ src/main/java/frc/robot/commands/elevator/LiftStop.java
@@ -0,0 +1,42 @@
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.elevator;
+
+import frc.robot.subsystems.Elevator;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class LiftStop extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final Elevator m_elevator;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public LiftStop(Elevator subsystem) {
+    m_elevator = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
