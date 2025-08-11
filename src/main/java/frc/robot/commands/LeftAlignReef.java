ccda2ddc0821323c0591f94a07812e122c8848e0
Zachary Barton <122578205+ZacAttackShack@users.noreply.github.com>
2025-08-11 18:39:01 -0400

    Added ID detection

---
+++ src/main/java/frc/robot/commands/LeftAlignReef.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.swervedrive.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/** An example command that uses an example subsystem. */
+public class LeftAlignReef extends Command {
+  @SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
+  private final SwerveSubsystem m_drive;
+  private final Vision m_vision;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public LeftAlignReef(SwerveSubsystem subsystem, Vision vision) {
+    m_drive = subsystem;
+    m_vision = vision;
+
+    
+      SmartDashboard.putNumber("Coral/Elevator/BiggestVisibleIdLeft", -1 );
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_vision.scanLeft();
+    if (m_drive.isRedAlliance()) {
+      int detectedId = 0;
+      if (detectedId >= 6 && detectedId <= 11) {
+
+      }
+    }
+    else {
+      int detectedId = 0;
+      if (detectedId >= 17 && detectedId <= 22) {
+
+      }
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
