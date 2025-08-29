6123496ef52b2abdeafaa9f7747c4653b9f6f7fb
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2025-08-29 00:34:39 -0400

    refactor

--- src/main/java/frc/robot/commands/ReefScoreCommand.java
+++ src/main/java/frc/robot/commands/ReefScoreCommand.java
@@ -33,7 +33,6 @@ public class ReefScoreCommand extends SequentialCommandGroup {
   private final SwerveSubsystem m_drive;
   private final ElevatorSubsystem m_elevator;
   private final Command m_outtakeWithSensor2;
-  private final DoubleSupplier m_reefSectorDegrees; // supplies heading to the chosen reef sector
   private final ReefSector m_sector;
   private final Setpoint m_elevatorSetpoint;
 
@@ -50,14 +49,12 @@ public class ReefScoreCommand extends SequentialCommandGroup {
       SwerveSubsystem drive,
       ElevatorSubsystem elevator,
       Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier,
       ReefSector sectorChoice,
       Setpoint elevatorSetpoint) {
 
     this.m_drive = drive;
     this.m_elevator = elevator;
     this.m_outtakeWithSensor2 = outtakeWithSensor2;
-    this.m_reefSectorDegrees = reefSectorDegreesSupplier;
     this.m_sector = sectorChoice;
     this.m_elevatorSetpoint = elevatorSetpoint;
 
@@ -146,100 +143,94 @@ public class ReefScoreCommand extends SequentialCommandGroup {
   public static String prettyDouble(double value) {
     return prettyDouble(value, 2);
   }
-public Double getReefSector(Pose2d currentPose){
-        Double centerXRed = 514.13 * 0.0254;
-        Double centerXBlue = 176.75 * 0.0254;
-        Double centerY = 158.5 * 0.0254;
 
-        Translation2d reefRelativePose = null;
-        
-        var alliance = DriverStation.getAlliance();
-        // return currentPose.getRotation().getDegrees();       
-        if(!alliance.isPresent()){
-            return null;
+  public Double getReefSector(Pose2d currentPose) {
+    Double centerXRed = 514.13 * 0.0254;
+    Double centerXBlue = 176.75 * 0.0254;
+    Double centerY = 158.5 * 0.0254;
 
-        } else if(alliance.get() == DriverStation.Alliance.Red){
-            reefRelativePose = new Translation2d(currentPose.getX() - centerXRed, currentPose.getY() - centerY);
-        
-        } else {
-            reefRelativePose = new Translation2d(currentPose.getX() - centerXBlue, currentPose.getY() - centerY);
-        }
+    Translation2d reefRelativePose = null;
 
-        double reefRelativeAngle = Math.toDegrees(Math.atan2(reefRelativePose.getY(), reefRelativePose.getX()));
-        reefRelativeAngle = ((reefRelativeAngle%360) + 360)%360;
-        SmartDashboard.putNumber("reefRelativeAngle:", reefRelativeAngle);
-        if(0 <= reefRelativeAngle && reefRelativeAngle <= 30){
-            return 0.0;
-        } else if(30 < reefRelativeAngle && reefRelativeAngle <= 90){
-            return 60.0;
-        } else if(90 < reefRelativeAngle && reefRelativeAngle <= 150){
-            return 120.0;
-        } else if(150 < reefRelativeAngle && reefRelativeAngle <= 210){
-            return 180.0;
-        } else if(210 < reefRelativeAngle && reefRelativeAngle <= 270){
-            return 240.0;
-        } else if(270 < reefRelativeAngle && reefRelativeAngle <= 330){
-            return 300.0;
-        } else if(330 < reefRelativeAngle && reefRelativeAngle <= 360){
-            return 0.0;
-        } else {
-            return null;
-        }
+    var alliance = DriverStation.getAlliance();
+    // return currentPose.getRotation().getDegrees();
+    if (!alliance.isPresent()) {
+      return null;
+
+    } else if (alliance.get() == DriverStation.Alliance.Red) {
+      reefRelativePose = new Translation2d(currentPose.getX() - centerXRed, currentPose.getY() - centerY);
+
+    } else {
+      reefRelativePose = new Translation2d(currentPose.getX() - centerXBlue, currentPose.getY() - centerY);
     }
+
+    double reefRelativeAngle = Math.toDegrees(Math.atan2(reefRelativePose.getY(), reefRelativePose.getX()));
+    reefRelativeAngle = ((reefRelativeAngle % 360) + 360) % 360;
+    SmartDashboard.putNumber("reefRelativeAngle:", reefRelativeAngle);
+    if (0 <= reefRelativeAngle && reefRelativeAngle <= 30) {
+      return 0.0;
+    } else if (30 < reefRelativeAngle && reefRelativeAngle <= 90) {
+      return 60.0;
+    } else if (90 < reefRelativeAngle && reefRelativeAngle <= 150) {
+      return 120.0;
+    } else if (150 < reefRelativeAngle && reefRelativeAngle <= 210) {
+      return 180.0;
+    } else if (210 < reefRelativeAngle && reefRelativeAngle <= 270) {
+      return 240.0;
+    } else if (270 < reefRelativeAngle && reefRelativeAngle <= 330) {
+      return 300.0;
+    } else if (330 < reefRelativeAngle && reefRelativeAngle <= 360) {
+      return 0.0;
+    } else {
+      return null;
+    }
+  }
 
   public static Command score(
       SwerveSubsystem drive,
       ElevatorSubsystem elevator,
       Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier,
       Setpoint setpoint,
       ReefSector sector) {
     return new ReefScoreCommand(
-        drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier, sector, setpoint);
+        drive, elevator, outtakeWithSensor2, sector, setpoint);
   }
 
   // L2
   public static Command scoreL2Left(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel2, ReefSector.LEFT);
   }
 
   public static Command scoreL2Right(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel2, ReefSector.RIGHT);
   }
 
   // L3
   public static Command scoreL3Left(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel3, ReefSector.LEFT);
   }
 
   public static Command scoreL3Right(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel3, ReefSector.RIGHT);
   }
 
   // L4
   public static Command scoreL4Left(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel4, ReefSector.LEFT);
   }
 
   public static Command scoreL4Right(
-      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2,
-      DoubleSupplier reefSectorDegreesSupplier) {
-    return score(drive, elevator, outtakeWithSensor2, reefSectorDegreesSupplier,
+      SwerveSubsystem drive, ElevatorSubsystem elevator, Command outtakeWithSensor2) {
+    return score(drive, elevator, outtakeWithSensor2,
         Setpoint.kLevel4, ReefSector.RIGHT);
   }
 }
